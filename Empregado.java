25932850de5c595bc302447a95f312e6c52a470a
TheMacioPL <vi.cingano@gmail.com>
2020-09-08 16:41:30 -0300

    Commit inicial, classe empregado foi criada

---
+++ Empregado.java
@@ -0,0 +1,17 @@
+public class empregado {
+    private String nome;
+    private int idade;
+    public String getNome() {
+        return nome;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+}
